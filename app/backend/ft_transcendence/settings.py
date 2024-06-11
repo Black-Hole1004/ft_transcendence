5a326102edfd64b60de5f053fc1689ed64710d38
mouadd55 <mouad_oudrib@outlook.com>
2024-06-11 19:09:03 +0100

    feat(backend): set up folder structure

--- app/backend/ft_transcendence/settings.py
+++ app/backend/ft_transcendence/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'ft_transcendence.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'ft_transcendence.wsgi.application'
 
 ADMIN_USERNAME=environ.get('ADMIN_USERNAME')
 ADMIN_PASSWORD=environ.get('ADMIN_PASSWORD')
