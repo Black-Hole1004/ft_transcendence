4ac94549090843935992c7cb07b50b22aa1b64b2
mouadd55 <mouad_oudrib@outlook.com>
2024-07-12 11:52:22 +0100

    feat: add achievements frame and badges

--- app/frontend/src/pages/Dashboard/Dashboard.jsx
+++ app/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -34,50 +34,11 @@ const Dashboard = () => {
 						<Leaderboard />
 					</div>
 				</div>
-				{/* <div className='lg:flex-1 ms:flex-none rightside-mt
-					lg:mr-modes-right-lg lg:ml-modes-left-lg ml:ml-modes-left-tb ml:mr-modes-right-tb'>
-					<div className='relative w-full lg:h-shapes-lg tb:h-shapes-tb font-dreamscape-sans text-primary select-none'>
-						<button className='absolute flex items-center hover:text-[rgba(0,0,0,0)] duration-500'>
-							<img
-								src='/assets/images/shape.svg'
-								className='brightness-[.5] hover:scale-[1.05] hover:brightness-100 transition duration-500
-								lg:h-tournaments-lg tb:h-tournaments-tb'
-								alt='tournament-background'
-							/>
-							<h1 className='absolute left-[5%] modes-font pointer-events-none'>
-								tournaments
-							</h1>
-						</button>
-						<button className='absolute right-0 hover:text-[rgba(0,0,0,0)] duration-500'>
-							<img
-								src='/assets/images/training.svg'
-								className='brightness-[.5] hover:scale-[1.05] hover:brightness-100 transition duration-500
-								lg:h-training-lg tb:h-training-tb'
-								alt='training-background'
-							/>
-							<h1 className='absolute top-[40%] right-[10%] modes-font pointer-events-none'>
-								training
-							</h1>
-						</button>
-						<button className='absolute lg:bottom-5 tb:bottom-2 right-0 transition hover:text-[rgba(0,0,0,0)] duration-500'>
-							<img
-								src='/assets/images/1v1.svg'
-								className='brightness-[.5] hover:scale-[1.05] hover:brightness-100 transition duration-500
-								lg:h-1vs1-lg tb:h-1vs1-tb'
-								alt='1vs1-background'
-							/>
-							<h1 className='absolute top-[40%] right-[30%] modes-font pointer-events-none'>
-								1 vs 1
-							</h1>
-						</button>
-					</div>
-				</div> */}
 				<div
-					className='lg:flex-1 ms:flex-none rightside-mt
+					className='flex flex-col flex-1 rightside-mt
 					lg:mr-modes-right-lg lg:ml-modes-left-lg ml:ml-modes-left-ms ml:mr-modes-right-ms'
 				>
 					<div className='relative w-full lg:h-shapes-lg ms:h-shapes-ms font-dreamscape-sans text-primary select-none modes'>
-
 						<button className='absolute hover:text-[rgba(0,0,0,0)] duration-500 w-[67%]'>
 							<img
 								src='/assets/images/shape.svg'
@@ -88,7 +49,6 @@ const Dashboard = () => {
 								tournaments
 							</h1>
 						</button>
-
 						<button className='absolute right-0 hover:text-[rgba(0,0,0,0)] duration-500 w-[42%]'>
 							<img
 								src='/assets/images/training.svg'
@@ -99,8 +59,10 @@ const Dashboard = () => {
 								training
 							</h1>
 						</button>
-						<button className='absolute right-0 transition hover:text-[rgba(0,0,0,0)] duration-500 w-[53%]
-							lg:bottom-2 lp:bottom-4 tb:bottom-3 ml:bottom-2 ms:bottom-1'>
+						<button
+							className='absolute right-0 transition hover:text-[rgba(0,0,0,0)] duration-500 w-[53%]
+							lg:bottom-2 lp:bottom-4 tb:bottom-3 ml:bottom-2 ms:bottom-1'
+						>
 							<img
 								src='/assets/images/1v1.svg'
 								className='brightness-[.5] hover:scale-[1.05] hover:brightness-100 transition duration-500'
@@ -111,7 +73,34 @@ const Dashboard = () => {
 							</h1>
 						</button>
 					</div>
-					{/* <div className='w-full h-96 border border-1.5 border-[rgba(255,206,157,.4)] rounded-xl'></div> */}
+					<div
+						className='border-1.5 border-[rgba(255,206,157,.4)] rounded-xl bg-[rgba(27,22,17,0.5)]
+						lg:h-achievements-lg achivements-padding'
+					>
+						<h1 className='font-dreamscape-sans text-primary text-center leading-[1.02] achivements'>Achivements</h1>
+						<div className='flex justify-between items-center text-[#FFCE9E]'>
+							<div className='text-center'>
+								<img src="./assets/images/Achievements/novice-astronaut.svg" alt="" />
+								<p className='achievements-titles-font font-dreamscape-sans'>Novice Astronaut</p>
+							</div>
+							<div className='relative text-center'>
+								<img src="./assets/images/Achievements/cosmic-explorer.svg" alt="" />
+								<p className='achievements-titles-font font-dreamscape-sans'>Cosmic Explorer</p>
+							</div>
+							<div className='relative text-center'>
+								<img src="./assets/images/Achievements/stellar-voyager.svg" alt="" />
+								<p className='achievements-titles-font font-dreamscape-sans'>Stellar Voyager</p>
+							</div>
+							<div className='relative text-center'>
+								<img src="./assets/images/Achievements/galactic-trailblazer.svg" alt="" />
+								<p className='achievements-titles-font font-dreamscape-sans'>Galactic Trailblazer</p>
+							</div>
+							<div className='relative text-center'>
+								<img src="./assets/images/Achievements/celestial-master.svg" alt="" />
+								<p className='achievements-titles-font font-dreamscape-sans'>Celestial Master</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 		</div>
