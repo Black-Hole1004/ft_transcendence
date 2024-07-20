2cecb236d12044cdd271cbdd48eab79bc0056967
mouadd55 <mouad_oudrib@outlook.com>
2024-07-20 08:34:44 +0100

    feat/Game: complete game customization page layout

--- app/frontend/src/pages/Game/Game.jsx
+++ app/frontend/src/pages/Game/Game.jsx
@@ -6,8 +6,8 @@ const Game = () => {
 	return (
 		<div className='min-h-screen flex flex-col backdrop-blur-sm bg-backdrop-40 text-primary'>
 			<Header />
-			<section className='flex-grow flex'>
-				<div className='page-margin flex-1 flex flex-col border border-border'>
+			<section className='flex-grow'>
+				<div className='page-margin flex flex-col border border-border'>
 					<div className='border border-victory flex justify-center'>
 						<div className='lp:border-2 border border-primary selected-table mtb:w-select-table w-full rounded-2xl
 							flex justify-center items-center'>
@@ -16,18 +16,19 @@ const Game = () => {
 							</button>
 						</div>
 					</div>
-					<div className='border border-defeat h-[15%] flex justify-center items-center'>
-						<h1 className='font-dreamscape-sans select-message'>select table</h1>
+					<div className='border border-defeat select-message flex justify-center items-center'>
+						<h1 className='font-dreamscape-sans'>select table</h1>
 					</div>
-					<div className='tables flex-1 flex border border-blue-600 gap-5'>
-						{/* <div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div>
-						<div className='flex-wrap w-[374px] border border-yellow-400'></div> */}
+					<div className='grid lg:gap-8 tb:gap-6 gap-4 border border-blue-600
+					lg:grid-cols-4 lg:grid-rows-2 lp:grid-cols-3 lp:grid-rows-3 grid-cols-2 grid-rows-4'>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
+						<div className='tables border border-yellow-400'></div>
 					</div>
 				</div>
 			</section>
