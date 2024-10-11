bc0915e3385d445cef3988c74597e7907106bee2
mouadd55 <mouad_oudrib@outlook.com>
2024-10-11 13:24:17 +0100

    feat/Chat: Create the WebSocket Consumer

--- app/backend/Chat/consumers.py
+++ app/backend/Chat/consumers.py
@@ -1,8 +1,37 @@
 import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-
 class ChatConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
-        
+        self.conversation_key = self.scope['url_route']['kwargs']['conversation_key']
+        print('conversation_key: ', self.conversation_key)
+        self.room_group_name = f'chat_{self.conversation_key}'
+        print('room_group: ', self.room_group_name)
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        print('Channel name: ', self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        print(f"===== Disconnected from {self.room_group_name} =====")
+
+    async def receive(self, text_data):
+        print(f"text_data: {text_data}")
+        data = json.loads(text_data)
+        print(f"data: {data}")
+        message = data['message']
+        print(f"message: {message}")
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {'type': 'chat.message', 'message': message}
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+        print('Message sent')
