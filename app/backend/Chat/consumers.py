11a27b7faf409160bbba023e79f3491368821f94
mouadd55 <mouad_oudrib@outlook.com>
2024-10-21 20:15:07 +0100

    feat/Chat: check user authentication in websocket connection

--- app/backend/Chat/consumers.py
+++ app/backend/Chat/consumers.py
@@ -1,15 +1,49 @@
 import json
 from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
+import jwt
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.conf import settings
 
 User = get_user_model()
+
+def extract_access_token(headers):
+    for header in headers:
+        key, value = header
+        if key == b'cookie':
+            cookies = value.decode()
+
+            cookie_list = cookies.split('; ')
+
+            for cookie in cookie_list:
+                if cookie.startswith('access_token='):
+                    return cookie.split('=')[1]
+    return None
+
+def decode_jwt_info(token):
+    try:
+        # Decode the JWT without verification (to get the payload/body)
+        payload = jwt.decode(
+            token, 
+            key=settings.SECRET_KEY,  # Use your Django secret key here
+            algorithms=["HS256"]      # Algorithm used for signing
+        )
+        return payload
+    except jwt.ExpiredSignatureError:
+        return {"error": "Token has expired"}
+    except jwt.InvalidTokenError:
+        return {"error": "Invalid token"}
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        print(self.scope)
-        self.user = self.scope['user']
+        print('*******************************************')
+        headers = self.scope['headers']
+        access_token = extract_access_token(headers)
+        print("Access Token:", access_token)
+        userid = decode_jwt_info(access_token)['user_id']
+        print('userid: ', userid)
+        self.user = await database_sync_to_async(User.objects.get)(id=userid)
         print(self.user)
         if self.user.is_authenticated:
             await self.accept()
