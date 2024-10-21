9ccbc6f89796ef973b7c67bf46a1eddf681f9099
mouadd55 <mouad_oudrib@outlook.com>
2024-10-21 15:11:37 +0100

    feat/Chat: working on creation groups for conversations

--- app/backend/Chat/consumers.py
+++ app/backend/Chat/consumers.py
@@ -1,35 +1,63 @@
 import json
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+User = get_user_model()
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
-        print('conversation_id: ', self.conversation_id)
-        self.room_group_name = f'chat_{self.conversation_id}'
-        print('room_group: ', self.room_group_name)
-
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        print('Channel name: ', self.channel_name)
-
-        await self.accept()
+        print(self.scope)
+        self.user = self.scope['user']
+        print(self.user)
+        if self.user.is_authenticated:
+            await self.accept()
+        else:
+            print('close')
+            await self.close()
+        print('==============================================')
 
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-        print(f"===== Disconnected from {self.room_group_name} =====")
+        # await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        print(f"===== Disconnected =====")
 
+    # @staticmethod
+    # def get_room_name(user1_id, user2_id):
+    #     return f"chat_{min(user1_id, user2_id)}_{max(user1_id, user2_id)}"
 
     async def receive(self, text_data):
         print(f"text_data: {text_data}")
         data = json.loads(text_data)
+        # sender = data['sender']
+        # receiver = data['receiver']
+        message_type = data['message_type']
         print(f"data: {data}")
-        message = data['message']
-        print(f"message: {message}")
+        # message = data['message']
+        # print(f"sender: {sender}")
+        # print(f"message: {message}")
+        # print(f"receiver: {receiver}")
 
-        await self.channel_layer.group_send(
-            self.room_group_name, {'type': 'chat.message', 'message': message}
-        )
+        if message_type == 'join':
+            # self.room_group_name = self.get_room_name(sender, receiver)
+            self.conversation_id = data['conversation_id']
+            print(f"conversation_id: {self.conversation_id}")
+            self.room_group_name = f"chat_{self.conversation_id}"
+            print('room_group_name: ', self.room_group_name)
+
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            print('joined group')
+
+        elif message_type == 'message':
+            sender = data['sender']
+            message = data['message']
+            print(f"sender: {sender}")
+            print(f"message: {message}")
+
+            await self.channel_layer.group_send(
+                self.room_group_name, {'type': 'chat.message', 'message': message}
+            )
 
 
     async def chat_message(self, event):
