0463be2b58c4ae690d01513d642ede89eff48e07
mouadd55 <mouad_oudrib@outlook.com>
2024-11-08 10:57:35 +0100

    feat/Chat: handle different user channel names

--- app/backend/Chat/consumers.py
+++ app/backend/Chat/consumers.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
 
+from django.core.cache import cache
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 User = get_user_model()
@@ -45,21 +47,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
         print('*******************************************')
         headers = self.scope['headers']
         access_token = extract_access_token(headers)
-        print("Access Token:", access_token)
+        # print("Access Token:", access_token)
         userid = decode_jwt_info(access_token)['user_id']
-        print('userid: ', userid)
+        # print('userid: ', userid)
         self.user = await database_sync_to_async(User.objects.get)(id=userid)
         print(self.user)
+        cache.set(f"user_{userid}_channel", self.channel_name)
         if self.user.is_authenticated:
             await self.accept()
         else:
             print('close')
             await self.close()
+        print('my_channel_name: ', self.channel_name)
         print('==============================================')
 
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        cache.delete(f"user_{self.user.id}_channel")
+        # if self.conversation_id:
+            # await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         print(f"===== Disconnected =====")
 
 
@@ -78,46 +84,69 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 
     async def receive(self, text_data):
-        print(f"text_data: {text_data}")
+        # print(f"text_data: {text_data}")
         data = json.loads(text_data)
         message_type = data['message_type']
         print(f"data: {data}")
-
+        print(self.channel_name)
         if message_type == 'join':
             self.conversation_id = data['conversation_id']
-            print(f"conversation_id: {self.conversation_id}")
-            self.room_group_name = f"chat_{self.conversation_id}"
-            print('room_group_name: ', self.room_group_name)
+            if self.conversation_id:
+                self.other_user = data['selected_user_id']
+                print('other_user: ', self.other_user)
+                print(f"conversation_id: {self.conversation_id}")
+                self.room_group_name = f"chat_{self.conversation_id}"
+                print('room_group_name: ', self.room_group_name)
 
-            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-            print('joined group')
+                await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+                print(f'my_id: {self.user.id} --- my_channel: {self.channel_name}')
+                # print('joined group')
+                
+                self.other_user_channel = cache.get(f"user_{self.other_user}_channel")
+                print(f'other_user_id: {self.other_user} --- other_user_channel: {self.other_user_channel}')
+                if self.other_user_channel:
+                    print('user_existed')
+                    await self.channel_layer.group_add(self.room_group_name, self.other_user_channel)
+                    
+                else:
+                    print('not existed')
 
         elif message_type == 'message':
-            sender = data['sender']
-            message = data['message']
-            conversation_id = data['conversation_id']
-            print(f"sender: {sender}")
-            print(f"message: {message}")
+            print('room: ', self.room_group_name)
+            if self.room_group_name:
+                sender = data['sender']
+                message = data['message']
+                conversation_id = data['conversation_id']
+                # print(f"sender: {sender}")
+                # print(f"message: {message}")
+                self.old_user_channel = self.other_user_channel
+                self.other_user_channel = cache.get(f"user_{self.other_user}_channel")
+                if self.old_user_channel is not self.other_user_channel:
+                    await self.channel_layer.group_add(self.room_group_name, self.other_user_channel)
 
-            print('======>', conversation_id)
-            saved_message = await self.save_message(
-                conversation_id = conversation_id,
-                sender_id = sender,
-                content = message
-            )
-            print(saved_message)
+                print(f'other_user_id: {self.other_user} --- other_user_channel: {self.other_user_channel}')
 
-            print('Message saved successfuly')
+                # print('======>', conversation_id)
+                saved_message = await self.save_message(
+                    conversation_id = conversation_id,
+                    sender_id = sender,
+                    content = message
+                )
+                # print(saved_message)
 
-            await self.channel_layer.group_send(
-                self.room_group_name, {
-                    'type': 'chat.message',
-                    'message': message,
-                    'sender': sender,
-                    'id': saved_message.id,
-                    'timestamp': saved_message.sent_datetime.isoformat()
-                }
-            )
+                # print('Message saved successfuly')
+
+                await self.channel_layer.group_send(
+                    self.room_group_name, {
+                        'type': 'chat.message',
+                        'message': message,
+                        'sender': sender,
+                        'id': saved_message.id,
+                        'timestamp': saved_message.sent_datetime.isoformat()
+                    }
+                )
+
+
 
 
 
@@ -129,4 +158,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'sender': event['sender'],
             'timestamp': event['timestamp']
         }))
-        print('Message sent')
+        # print('Message sent')
