45fb5b1b2bc5e814059da2b508bba75215428dd0
AYHALOUI <aymenhaoui00@gmail.com>
2024-12-16 01:21:14 +0100

    brackets done

--- app/frontend/src/pages/Tournament/Tournament.jsx
+++ app/frontend/src/pages/Tournament/Tournament.jsx
@@ -15,7 +15,7 @@ const players = [
 		rank: "CELESTIAL MASTER",
 		rankClass: "text-orange-400",
 		xp: 11648,
-		avatar: '../../../dist/assets/images/moudrib.jpeg',
+		avatar: '../../../dist/assets/images/tabi3a.jpeg',
 		icon: "🔥"
 	},
 	{
@@ -33,77 +33,156 @@ const players = [
 		rank: "GALACTIC TRAILBLAZER",
 		rankClass: "text-cyan-400",
 		xp: 9153,
-		avatar: '../../../dist/assets/images/moudrib.jpeg',
+		avatar: '../../../dist/assets/images/ahaloui.jpeg',
 		icon: "🌍"
 	},
 ]
+
 const Tournament = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<div className="border border-gray-300 p-4 rounded-md w-[800px] h-[800px]">
+		<div className="flex flex-col items-center justify-center h-screen ">
 
-				<div className="flex flex-col items-start justify-between h-full space-y-4">
-
-					<div className=' flex-1 flex flex-col justify-around'>
-						
-						<div className='flex flex-row relative'>
+			<div className="flex flex-col border border-gray-300 p-4 rounded-md w-[800px] h-[800px]">
+				
+				{/* first partie of tournament */}
+				<div className=' flex-1 flex flex-row relative'>
+					{/* first column */}
+					<div className=' flex flex-col justify-around'>
+						<div className=''> 							
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[0].avatar}
 								alt="avatar"
 							/>
-							<div className='border-t-2 border-white h-24 w-24 flex justify-center items-center border-white border-t-2 border-r-2 absolute top-12 left-24'>
-							</div>
-
-							<div className='h-24 w-24 border-t-2 border-white absolute top-[140px] left-48 flex justify-center items-center'>
-							</div>
 						</div>
 
-
-						<div className='flex flex-row relative'>
+						<div className=''> 							
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
-								src={players[0].avatar}
+								src={players[1].avatar}
 								alt="avatar"
 							/>
-							<div className=' h-24 w-24 flex justify-center items-center border-white border-b-2 border-r-2 absolute -top-12 left-24'>
-							</div>
+						</div>
+					</div>
+					{/* second column */}
+					<div className='w-24 h-full flex flex-col'>
+						<div className=' flex-1'>
+						</div>
+						<div className='border-t-2 border-r-2 border-white flex-1'>
+						</div>
+						<div className='border-b-2 border-r-2 border-white flex-1'>
+						</div>
+						<div className='flex-1'>
 						</div>
 					</div>
 
-					<div className=' flex-1 flex flex-col justify-around'>
-						<div className='flex flex-row relative'>
+					{/* third column */}
+					<div className='w-24 flex flex-col'>
+						<div className='flex-1'></div>
+						<div className='flex-1'></div>
+						<div className='border-t-2 border-white flex-1'></div>
+						<div className='flex-1'> </div>
+					</div>
+
+					{/* fourth column */}
+					<div className=' w-24 flex flex-col'>
+						<div className=' flex-1'></div>
+						<div className=' flex-1 flex justify-center items-center'>
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[0].avatar}
 								alt="avatar"
 							/>
-							<div className='border-t-2 border-white h-24 w-24 flex justify-center items-center border-white border-t-2 border-r-2 absolute top-12 left-24'>
-							</div>
-							
-							<div className='h-24 w-24 border-t-2 border-white absolute top-[140px] left-48 flex justify-center items-center'>
-							</div>
-							{/* <img
-								className="w-24 h-24 rounded-full border-2 border-white"
-								src={players[0].avatar}
-								alt="avatar"
-							/> */}
 						</div>
-						<div className='flex flex-row relative'>
+						<div className=' flex-1'> </div>
+					</div>
+
+					{/* fifth column */}
+					<div className='w-24 h-full flex flex-col'>
+						<div className=' flex-1'></div>
+						<div className='flex-1'></div>
+						<div className='border-t-2 border-r-2 border-white flex-1'></div>
+						<div className='border-r-2 flex-1'></div>
+					</div>
+
+					{/* sixth column */}
+					<div className='border-b border-white w-24 h-full flex flex-col'></div>
+
+					{/* seventh column */}
+					<div className='w-24 h-full flex flex-col absolute top-1/2 right-24 justify-center '>
+						<img
+							className="w-24 h-24 rounded-full border-2 border-white"
+							src={players[0].avatar}
+							alt="avatar"
+						/>
+					</div>
+				</div>
+
+
+				{/* second partie of tournament */}
+				<div className=' flex-1 flex flex-row'>
+					{/* first column */}
+					<div className=' flex flex-col justify-around'>
+						<div className=''> 							
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
-								src={players[0].avatar}
+								src={players[2].avatar}
 								alt="avatar"
 							/>
-							<div className=' h-24 w-24 flex justify-center items-center border-white border-b-2 border-r-2 absolute -top-12 left-24'>
-							</div>
-							
+						</div>
+
+						<div className=''> 							
+							<img
+								className="w-24 h-24 rounded-full border-2 border-white"
+								src={players[3].avatar}
+								alt="avatar"
+							/>
 						</div>
 					</div>
+					{/* second column */}
+					<div className='w-24 h-full flex flex-col'>
+						<div className=' flex-1'></div>
+						<div className='border-t-2 border-r-2 border-white flex-1'></div>
+						<div className='border-b-2 border-r-2 border-white flex-1'></div>
+						<div className='flex-1'></div>
+					</div>
+
+					{/* third column */}
+					<div className='w-24 flex flex-col'>
+						<div className='flex-1'></div>
+						<div className='flex-1'></div>
+						<div className='border-t-2 border-white flex-1'></div>
+						<div className='flex-1'> </div>
+					</div>
+
+					{/* fourth column */}
+					<div className=' w-24 flex flex-col'>
+						<div className=' flex-1'></div>
+						<div className=' flex-1 flex justify-center items-center'>
+							<img
+								className="w-24 h-24 rounded-full border-2 border-white"
+								src={players[3].avatar}
+								alt="avatar"
+							/>
+						</div>
+						<div className=' flex-1'> </div>
+					</div>
+
+					{/* fifth column */}
+					<div className=' w-24 h-full flex flex-col'>
+						<div className='border-r-2 flex-1'></div>
+						<div className='border-b-2 border-r-2 flex-1'></div>
+						<div className=' border-white flex-1'></div>
+						<div className='flex-1'></div>
+					</div>
+
+					{/* sixth column */}
+					<div className='border-t border-white w-24 h-full flex flex-col'>
+					</div>
+					
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
-
 export default Tournament;
