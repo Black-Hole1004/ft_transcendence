3ea6b153baacc8a20b22cce634951de708de2307
AYHALOUI <aymenhaoui00@gmail.com>
2024-12-16 18:04:58 +0100

    brackets done

--- app/frontend/src/pages/Tournament/Tournament.jsx
+++ app/frontend/src/pages/Tournament/Tournament.jsx
@@ -1,54 +1,155 @@
 import React from 'react';
+import Button from '../../components/Home/Buttons/Button';
+
 const players = [
 	{
 		id: "1",
-		name: "MOUAD55",
-		rank: "CELESTIAL MASTER",
+		nickname: "MOUAD55",
+		achievement: "CELESTIAL MASTER",
 		rankClass: "text-orange-400",
 		xp: 12456,
 		avatar: '../../../dist/assets/images/moudrib.jpeg',
-		icon: "🔥"
+		icon: '../../../dist/assets/images/Achievements/celestial-master.png'
 	},
 	{
 		id: "2",
-		name: "ARABIAI",
-		rank: "CELESTIAL MASTER",
+		nickname: "ARABIAI",
+		achievement: "CELESTIAL MASTER",
 		rankClass: "text-orange-400",
 		xp: 11648,
 		avatar: '../../../dist/assets/images/tabi3a.jpeg',
-		icon: "🔥"
+		icon: '../../../dist/assets/images/Achievements/galactic-trailblazer.png'
 	},
 	{
 		id: "3",
-		name: "AHMAYMOU",
-		rank: "CELESTIAL MASTER",
+		nickname: "AHMAYMOU",
+		achievement: "CELESTIAL MASTER",
 		rankClass: "text-orange-400",
 		xp: 10231,
 		avatar: '../../../dist/assets/images/lmoudir.jpg',
-		icon: "🔥"
+		icon: '../../../dist/assets/images/Achievements/stellar-voyager.png'
 	},
 	{
 		id: "4",
-		name: "PLAYER1",
-		rank: "GALACTIC TRAILBLAZER",
+		nickname: "PLAYER1",
+		achievement: "GALACTIC TRAILBLAZER",
 		rankClass: "text-cyan-400",
 		xp: 9153,
 		avatar: '../../../dist/assets/images/ahaloui.jpeg',
-		icon: "🌍"
+		icon: '../../../dist/assets/images/Achievements/cosmic-explorer.png'
 	},
+	{
+		id: "1",
+		nickname: "MOUAD55",
+		achievement: "CELESTIAL MASTER",
+		rankClass: "text-orange-400",
+		xp: 12456,
+		avatar: '../../../dist/assets/images/moudrib.jpeg',
+		icon: '../../../dist/assets/images/Achievements/celestial-master.png'
+	},
+	{
+		id: "2",
+		nickname: "ARABIAI",
+		achievement: "CELESTIAL MASTER",
+		rankClass: "text-orange-400",
+		xp: 11648,
+		avatar: '../../../dist/assets/images/tabi3a.jpeg',
+		icon: '../../../dist/assets/images/Achievements/galactic-trailblazer.png'
+	},
+	{
+		id: "3",
+		nickname: "AHMAYMOU",
+		achievement: "CELESTIAL MASTER",
+		rankClass: "text-orange-400",
+		xp: 10231,
+		avatar: '../../../dist/assets/images/lmoudir.jpg',
+		icon: '../../../dist/assets/images/Achievements/stellar-voyager.png'
+	},
+	{
+		id: "4",
+		nickname: "PLAYER1",
+		achievement: "GALACTIC TRAILBLAZER",
+		rankClass: "text-cyan-400",
+		xp: 9153,
+		avatar: '../../../dist/assets/images/ahaloui.jpeg',
+		icon: '../../../dist/assets/images/Achievements/cosmic-explorer.png'
+	},
+
 ]
 
 const Tournament = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen ">
+		<div className="flex flex-row items-start justify-center min-h-screen overflow-hidden">
 
-			<div className="flex flex-col border border-gray-300 p-4 rounded-md w-[800px] h-[800px]">
-				
+			<div className="w-[35%] flex flex-col overflow-hidden  p-8">
+				<div className=''>
+					<h1 className='text-6xl font-dreamscape'
+						style={{
+							textShadow: '0 0 10px rgba(255,255,255,0.5), 0 0 20px rgba(255,255,255,0.3)',
+						}}
+					>
+						CELESTIAL PONG CLASH
+					</h1>
+					<div className='flex flex-col justify-center'>
+						<p className='text-gray-300 text-base font-medium text-justify pr-40'>
+							The Celestial Pong Clash invites players from across the galaxy to compete in
+							intense interstellar battles, where victory depends on mastering precision and
+							strategy in the vast cosmic realm.
+						</p>
+					</div>
+				</div>
+
+				<div className='justify-center rounded-3xl mt-20 w-[60%] border border-white border-b-opacity-20'>
+					<h2 className='text-2xl font-semibold flex items-center justify-center'>PLAYERS</h2>
+					<div className="space-y-4 p-4 ">
+						{
+							players.map((player, index) => {
+								return (
+									<div
+										key={index}
+										className='user-container flex items-center justify-between font-dreamscape-sans
+									rounded-md hover:bg-[rgba(183,170,156,0.2)]'>
+										<div className='h-full flex items-center xl:gap-3 tb:gap-2 gap-1 w-[68%]'>
+											<img
+												src={player.avatar}
+												className='h-16 rounded-full ring-1 ring-primary select-none'
+												alt='user-image'
+												loading='eager'
+											/>
+											<div className='flex flex-wrap items-center overflow-hidden'>
+												<p className='text-primary nickname-size leading-[1] truncate mr-1'>
+													{player.nickname}
+												</p>
+												<p className='text-achievement text-xs '> {player.achievement}</p>
+											</div>
+										</div>
+										<div>
+
+										</div>
+										<div className='h-full mx-1 flex items-center'>
+											<img
+												src={player.icon}
+												className='h-16 select-none'
+												alt='achievement-icon'
+												loading='eager'
+											/>
+											<p className={`xp text-primary leading-[1]`}>{`${player.xp}` + 'xp'}</p>
+										</div>
+									</div>
+								)
+							}
+							)
+						}
+					</div>
+				</div>
+			</div>
+
+			<div className="flex flex-col border rounded-3xl mt-20 pb-8 pl-16 w-[800px] h-[1100px]">
 				{/* first partie of tournament */}
 				<div className=' flex-1 flex flex-row relative'>
 					{/* first column */}
 					<div className=' flex flex-col justify-around'>
-						<div className=''> 							
+						<div className=''>
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[0].avatar}
@@ -56,7 +157,7 @@ const Tournament = () => {
 							/>
 						</div>
 
-						<div className=''> 							
+						<div className=''>
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[1].avatar}
@@ -66,14 +167,10 @@ const Tournament = () => {
 					</div>
 					{/* second column */}
 					<div className='w-24 h-full flex flex-col'>
-						<div className=' flex-1'>
-						</div>
-						<div className='border-t-2 border-r-2 border-white flex-1'>
-						</div>
-						<div className='border-b-2 border-r-2 border-white flex-1'>
-						</div>
-						<div className='flex-1'>
-						</div>
+						<div className=' flex-1'></div>
+						<div className='border-t-2 border-r-2 border-white flex-1'></div>
+						<div className='border-b-2 border-r-2 border-white flex-1'></div>
+						<div className='flex-1'></div>
 					</div>
 
 					{/* third column */}
@@ -123,7 +220,7 @@ const Tournament = () => {
 				<div className=' flex-1 flex flex-row'>
 					{/* first column */}
 					<div className=' flex flex-col justify-around'>
-						<div className=''> 							
+						<div className=''>
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[2].avatar}
@@ -131,7 +228,7 @@ const Tournament = () => {
 							/>
 						</div>
 
-						<div className=''> 							
+						<div className=''>
 							<img
 								className="w-24 h-24 rounded-full border-2 border-white"
 								src={players[3].avatar}
@@ -179,9 +276,18 @@ const Tournament = () => {
 					{/* sixth column */}
 					<div className='border-t border-white w-24 h-full flex flex-col'>
 					</div>
-					
+				</div>
+
+				<div className='flex justify-center'>
+					<Button className={
+						'rounded-md  w-1/2 h-12 font-regular buttons-text remove-button'
+					}
+						type='submit' >
+						Start Tournament
+					</Button>
 				</div>
 			</div>
+
 		</div>
 	)
 }
