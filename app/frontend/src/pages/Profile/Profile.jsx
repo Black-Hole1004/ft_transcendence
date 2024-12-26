8202f566a9a1bd9ac1af5fb46f89c652603571a2
AYHALOUI <aymenhaoui00@gmail.com>
2024-12-26 17:57:39 +0100

    fix the joined date in the About section (profile) and add search in friendslist

--- app/frontend/src/pages/Profile/Profile.jsx
+++ app/frontend/src/pages/Profile/Profile.jsx
@@ -83,6 +83,7 @@ const Profile = () => {
 	const [display_name, setDisplay_name] = useState('')
 	const [bio, setBio] = useState('')
 	const [profile_picture, setProfile_picture] = useState('')
+	const [date_joined_formatted, setDate_joined_formatted] = useState('')
 
 
 	const [user, setUser] = useState({
@@ -94,6 +95,7 @@ const Profile = () => {
 		display_name: '',
 		bio: '',
 		profile_picture: '',
+		date_joined_formatted: ''
 	})
 
 	const fetchUser = async () => {
@@ -138,7 +140,10 @@ const Profile = () => {
 		setDisplay_name(user.display_name);
 		setBio(user.bio);
 		setProfile_picture(user.profile_picture);
+		setDate_joined_formatted(user.date_joined_formatted);
 	}, [user]);
+
+	console.log('user --------->', user);
 
 	/************************************************************************ */
 
@@ -167,7 +172,7 @@ const Profile = () => {
 						className='lp:ml-about-lp flex font-medium mtb:flex-row flex-col lp:justify-start mtb:justify-around
 						xl:gap-20 lg:gap-10 gap-3 max-lp:ml-0 mt-2'
 					>
-						<AboutSection first_name={first_name} last_name={last_name} email={email} mobile_number={mobile_number} username={username} display_name={display_name} bio={bio} />
+						<AboutSection first_name={first_name} last_name={last_name} email={email} mobile_number={mobile_number} username={username} display_name={display_name} bio={bio} data_joined_formatted={date_joined_formatted} />
 						<div className='flex flex-col items-center gap-2'>
 							<p className='titles max-mtb:self-start max-mtb:ml-3'>
 								Overall Progression
